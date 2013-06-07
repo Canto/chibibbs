a445cc4084fe4ab3a4a69d7a60f5f7d1f837efae
Yoon JoonYoung <canto87@naver.com>
2013-06-07 10:55:33 +0900

    revision 24 admin.board.delete.php > intent 수정

--- chibibbs1.10/admin/admin.board.delete.php
+++ chibibbs1.10/admin/admin.board.delete.php
@@ -5,26 +5,26 @@ $connect_page=true;
 $cid = $_GET['cid'];
 
 if($member->permission=="all" || $member->permission=="super"){
-$member_permission = "all";
-$error='';
-$sql = "DELETE FROM `chibi_admin` WHERE `cid`='".mysql_real_escape_string($cid)."'";
-mysql_query($sql,$chibi_conn);
-$error = mysql_error();
-if(empty($error)==true){
-$sql2 = "DELETE FROM `chibi_emoticon` WHERE `cid`='".mysql_real_escape_string($cid)."'";
-mysql_query($sql2,$chibi_conn);
-$sql3 = "DELETE FROM `chibi_skin` WHERE `cid`='".mysql_real_escape_string($cid)."'";
-mysql_query($sql3,$chibi_conn);
-$sql4 = "DELETE FROM `chibi_tpl` WHERE `cid`='".mysql_real_escape_string($cid)."'";
-mysql_query($sql4,$chibi_conn);
-$sql5 = "DELETE FROM `chibi_pic` WHERE `cid`='".mysql_real_escape_string($cid)."'";
-mysql_query($sql5,$chibi_conn);
-$sql6 = "DELETE FROM `chibi_comment` WHERE `cid`='".mysql_real_escape_string($cid)."'";
-mysql_query($sql6,$chibi_conn);
-rmdir_rf("../data/".$cid."/emoticon");
-unlink("../data/".$cid."/tpl/".$cid.".tpl.php");
-rmdir_rf("../data/".$cid);
-}
+	$member_permission = "all";
+	$error='';
+	$sql = "DELETE FROM `chibi_admin` WHERE `cid`='".mysql_real_escape_string($cid)."'";
+	mysql_query($sql,$chibi_conn);
+	$error = mysql_error();
+	if(empty($error)==true){
+		$sql2 = "DELETE FROM `chibi_emoticon` WHERE `cid`='".mysql_real_escape_string($cid)."'";
+	mysql_query($sql2,$chibi_conn);
+	$sql3 = "DELETE FROM `chibi_skin` WHERE `cid`='".mysql_real_escape_string($cid)."'";
+	mysql_query($sql3,$chibi_conn);
+	$sql4 = "DELETE FROM `chibi_tpl` WHERE `cid`='".mysql_real_escape_string($cid)."'";
+	mysql_query($sql4,$chibi_conn);
+	$sql5 = "DELETE FROM `chibi_pic` WHERE `cid`='".mysql_real_escape_string($cid)."'";
+	mysql_query($sql5,$chibi_conn);
+	$sql6 = "DELETE FROM `chibi_comment` WHERE `cid`='".mysql_real_escape_string($cid)."'";
+	mysql_query($sql6,$chibi_conn);
+	rmdir_rf("../data/".$cid."/emoticon");
+	rmdir_rf("../data/".$cid."/tpl");
+	rmdir_rf("../data/".$cid);
+	}
 }else{
 $connect_page=false;
 }
