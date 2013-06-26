99b403b89ee8d1dc07274c45ac5a6d9743d6e90e
canto <canto87@naver.com>
2013-06-26 19:16:44 +0900

    commit #61 메모검색추가

--- chibibbs1.10/index.php
+++ chibibbs1.10/index.php
@@ -91,6 +91,9 @@ if(empty($_GET['cid'])==false){
 		case "comment" :
 			$sql = "SELECT count(`chibi_pic`.`idx`) FROM  `chibi_pic` LEFT JOIN  `chibi_comment` ON  `chibi_pic`.`no` =  `chibi_comment`.`pic_no` WHERE  `chibi_comment`.`comment` LIKE '%".mysql_real_escape_string($keyword)."%' AND  `chibi_comment`.`cid` =  '".mysql_real_escape_string($cid)."' GROUP BY `chibi_pic`.`no` ORDER BY  `chibi_pic`.`no` DESC";
 			break;
+		case "memo" :
+			$sql = "SELECT count(`chibi_pic`.`idx`) FROM  `chibi_pic` LEFT JOIN  `chibi_comment` ON  `chibi_pic`.`no` =  `chibi_comment`.`pic_no` WHERE  `chibi_comment`.`memo` LIKE '%".mysql_real_escape_string($keyword)."%' AND  `chibi_comment`.`cid` =  '".mysql_real_escape_string($cid)."' GROUP BY `chibi_pic`.`no` ORDER BY  `chibi_pic`.`no` DESC";
+			break;
 		case "no" : 
 			$sql = "SELECT count(no) FROM `chibi_pic` WHERE cid='".mysql_real_escape_string($cid)."' AND no='".mysql_real_escape_string($keyword)."'";
 			break;
